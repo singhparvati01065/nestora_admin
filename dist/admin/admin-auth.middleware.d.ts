d6c099c788c41fcb9eed85513e34a0b13cbe47ff
Parvati <rajdeep.shah@theranow.com>
2026-07-25 20:54:45 +0530

    Nestora super-admin panel (NestJS + Prisma + Handlebars)

--- dist/admin/admin-auth.middleware.d.ts
+++
@@ -1,8 +0,0 @@
-import { NestMiddleware } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { NextFunction, Request, Response } from 'express';
-export declare class AdminAuthMiddleware implements NestMiddleware {
-    private jwt;
-    constructor(jwt: JwtService);
-    use(req: Request, res: Response, next: NextFunction): void;
-}
